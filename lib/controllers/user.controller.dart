f9c1edb8686d383b6769e799bfdd28f23ca8e26f
Tai521h0481 <521h0481@student.tdtu.edu.vn>
2024-01-23 18:55:08 +0700

    complete login

---
+++ lib/controllers/user.controller.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+String baseUrl = "http://192.168.0.111:3000";
+String register = baseUrl + "/android/users/register";
+String login = baseUrl + "/android/users/login";
+
+Future<Map<String, dynamic>> registerAPI({required email, required password}) async {
+  var response = await http.post(
+    Uri.parse(register),
+    headers: {"Content-Type": "application/json"},
+    body: jsonEncode({
+      "email": email,
+      "password": password,
+    }),
+  );
+
+  final Map<String, dynamic> data = json.decode(response.body);
+  return data;
+}
+
+Future<Map<String, dynamic>> loginAPI({required email, required password}) async {
+  var response = await http.post(
+    Uri.parse(login),
+    headers: {"Content-Type": "application/json"},
+    body: jsonEncode({
+      "email": email,
+      "password": password,
+    }),
+  );
+
+  final Map<String, dynamic> data = json.decode(response.body);
+  return data;
+}
